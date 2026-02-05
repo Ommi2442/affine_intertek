67daae8914d822bcd4f4876f273c976b56e3aa00
Akshay <akshay.revankar@affine.ai>
2026-02-05 13:07:08 +0530

    worker file changes

--- Backend/worker_main.py
+++ Backend/worker_main.py
@@ -5,16 +5,15 @@ from concurrent.futures import ThreadPoolExecutor
 from projects.trf_processor import process_trf_direct
 from projects.letter_processor import process_letter_direct
 from typing import Optional
-
-
 from projects.cdr_processor import process_cdr_direct
 
 app = FastAPI(title="TRF CDR Letter Workers")
 
 
+TRF_EXECUTOR = ThreadPoolExecutor(max_workers=2)
+CDR_EXECUTOR = ThreadPoolExecutor(max_workers=2)
+LETTER_EXECUTOR = ThreadPoolExecutor(max_workers=2)
 
-EXECUTOR = ThreadPoolExecutor(max_workers=4)
-CDR_EXECUTOR = ThreadPoolExecutor(max_workers=6)
 
 class TRFJob(BaseModel):
     projectId: str
@@ -22,13 +21,19 @@ class TRFJob(BaseModel):
 class CDRJob(BaseModel):
     projectId: str
 
+class LetterJob(BaseModel):
+    projectId: str
+    trf_urls: Optional[str] = None
+    cdr_urls: Optional[str] = None
+
 
 @app.post("/run-trf", status_code=202)
 async def run_trf(job: TRFJob):
     try:
+        print("----- TRF Worker Started -------")
         loop = asyncio.get_running_loop()
         loop.run_in_executor(
-            EXECUTOR,
+            TRF_EXECUTOR,
             process_trf_direct,
             job.projectId
         )
@@ -46,7 +51,7 @@ async def run_trf(job: TRFJob):
 @app.post("/run-cdr", status_code=202)
 async def run_trf(job: CDRJob):
     try:
-        print("----- CDR worker-------")
+        print("----- CDR Worker Started-------")
         loop = asyncio.get_running_loop()
         loop.run_in_executor(
             CDR_EXECUTOR,
@@ -62,29 +67,17 @@ async def run_trf(job: CDRJob):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-
-
-
-
-
 # -------------------------
 # LETTER JOB
 # -------------------------
 
-class LetterJob(BaseModel):
-    projectId: str
-    trf_urls: Optional[str] = None
-    cdr_urls: Optional[str] = None
-
-
-
 @app.post("/run-letter", status_code=202)
 async def run_letter(job: LetterJob):
-    print('**** run_letter *****', job.trf_urls)
     try:
+        print("----- LETTER Worker Started-------")
         loop = asyncio.get_running_loop()
         loop.run_in_executor(
-            EXECUTOR,
+            LETTER_EXECUTOR,
             process_letter_direct,
             job.projectId,
             job.trf_urls,
